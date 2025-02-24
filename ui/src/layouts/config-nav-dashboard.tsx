cd43c0c7ea22c7a3e4494055d7f2981a50510e5a
Vaishvi Verma <132043950+vaishviverma@users.noreply.github.com>
2025-02-24 13:11:39 +0530

    commit

---
+++ ui/src/layouts/config-nav-dashboard.tsx
@@ -0,0 +1,18 @@
+import { Label } from 'src/components/label';
+import { SvgColor } from 'src/components/svg-color';
+
+// ----------------------------------------------------------------------
+
+const icon = (name: string) => (
+  <SvgColor width="100%" height="100%" src={`/assets/icons/navbar/${name}.svg`} />
+);
+
+export const navData = [
+  {
+    title: 'Dashboard',
+    path: '/',
+    icon: icon('ic-analytics'),
+  },
+  
+  
+];
---
+++ ui/src/routes/sections.tsx
@@ -0,0 +1,47 @@
+import { lazy, Suspense } from 'react';
+import { Outlet, Navigate, useRoutes } from 'react-router-dom';
+
+import Box from '@mui/material/Box';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+
+import { varAlpha } from 'src/theme/styles';
+import { DashboardLayout } from 'src/layouts/dashboard';
+
+// ----------------------------------------------------------------------
+
+export const HomePage = lazy(() => import('src/pages/home'));
+
+// ----------------------------------------------------------------------
+
+const renderFallback = (
+  <Box display="flex" alignItems="center" justifyContent="center" flex="1 1 auto">
+    <LinearProgress
+      sx={{
+        width: 1,
+        maxWidth: 320,
+        bgcolor: (theme) => varAlpha(theme.vars.palette.text.primaryChannel, 0.16),
+        [`& .${linearProgressClasses.bar}`]: { bgcolor: 'text.primary' },
+      }}
+    />
+  </Box>
+);
+
+export function Router() {
+  return useRoutes([
+    {
+      element: (
+        <DashboardLayout>
+          <Suspense fallback={renderFallback}>
+            <Outlet />
+          </Suspense>
+        </DashboardLayout>
+      ),
+      children: [
+        { element: <HomePage />, index: true },
+
+      ],
+    },
+    
+    
+  ]);
+}
